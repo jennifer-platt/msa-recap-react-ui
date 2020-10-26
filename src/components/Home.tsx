4afa866b9721eb4c85ed42b648492f64961c2762
jennifer-platt <jcp215@exeter.ac.uk>
2020-10-26 22:40:06 +0000

    Conditional rendering

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { useState } from "react";
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import TextField from '@material-ui/core/TextField';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import PauseIcon from '@material-ui/icons/Pause';
 import StopIcon from '@material-ui/icons/Stop';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Tooltip from '@material-ui/core/Tooltip';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
 
 import Viewer from "./Viewer";
 
@@ -27,20 +22,29 @@ import { projectName, uploadBlob } from "../azure_upload";
 
 // const logArray = Array(<></>);
 
-function Home() {
-
-  const [disabled, setDisabled] = useState({ enable: false, start: true, pause: true, end: true, upload: true, download: true });
-
-  const [filename, setFilename] = useState("recording");
-
-  const [project, setProject] = useState('');
-
+export default class Home extends React.Component<{}, {
+  recorded: boolean,
+  disabled: any,
+  filename: string,
+  project: string,
+  edited: boolean
+  }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      recorded: false,
+      disabled: { enable: false, start: true, pause: true, end: true, upload: true, download: true },
+      filename: "recording",
+      project: "",
+      edited: false
+    };
+  }
 
   //setDisabled({enable:false,start:true,pause:true,end:true,upload:true,download:true});
 
-  function download() {
-    downloadScreenCapture(filename);
-    downloadAudioCapture(filename);
+  download() {
+    downloadScreenCapture(this.state.filename);
+    downloadAudioCapture(this.state.filename);
   }
 
   /*
@@ -91,102 +95,90 @@ function Home() {
   }
   */
 
-  let enableRecording = () => {
+  enableRecording() {
     projectName().then((name) => {
-      setProject(name);
+      this.setState({ project: name });
     }).catch((err) => { console.warn(err) });
     // enableMicrophone();
     // enableScreenCap();
-    setDisabled({ enable: true, start: false, pause: true, end: true, upload: true, download: true });
+    this.setState({ disabled: { enable: true, start: false, pause: true, end: true, upload: true, download: true } });
+    console.log(this.state);
   }
 
-  let startRecording = () => {
+  startRecording() {
     startAudioCapture();
     startScreenCapture();
-    setDisabled({ enable: true, start: true, pause: false, end: false, upload: true, download: true });
+    this.setState({ disabled: { enable: true, start: true, pause: false, end: false, upload: true, download: true } });
   }
 
-  let pauseRecording = () => {
+  pauseRecording() {
     pauseAudioCapture();
     pauseScreenCapture();
-    setDisabled({ enable: true, start: false, pause: true, end: false, upload: true, download: true });
+    this.setState({ disabled: { enable: true, start: false, pause: true, end: false, upload: true, download: true } });
   }
 
-  function endRecording() {
+  endRecording() {
     stopAudioCapture();
     stopScreenCapture();
     disableMicrophone();
     disableScreenCap();
-    setDisabled({ enable: false, start: true, pause: true, end: true, upload: false, download: false });
-    upload();
+
+    this.setState({ disabled: { enable: false, start: true, pause: true, end: true, upload: false, download: false } });
+    this.upload();
   }
 
-  function upload() {
+  upload() {
 
-    if (project) {
+    if (this.state.project) {
       console.info("Calling upload()");
       let ablob: Blob = getAudioCaptureBlob();
-      uploadBlob(ablob, project, "ogg", true).then((m) => {
+      uploadBlob(ablob, this.state.project, "ogg", true).then((m) => {
         console.warn("Upload message", m);
       });
       let vblob: Blob = getCaptureBlob();
-      uploadBlob(vblob, project, "webm", false).then((m) => {
+      uploadBlob(vblob, this.state.project, "webm", false).then((m) => {
         console.warn("Upload message", m);
       });
+      this.setState({ recorded: true });
     }
 
   }
 
-  return (
-    <div className="App">
-      <br /><br />
-      <p>
-        <Button variant="contained" color="primary" id="enableRecording" disabled={disabled.enable} onClick={enableRecording}>Enable Recording</Button>
-      </p>
-      <p>
-        <ButtonGroup variant="contained" color="primary">
-          <Button id="start" disabled={disabled.start} onClick={startRecording}><Tooltip title="Start Recording" aria-label="Start Recording" arrow><FiberManualRecordIcon /></Tooltip></Button>
-          <Button id="pause" disabled={disabled.pause} onClick={pauseRecording}><Tooltip title="Pause Recording" aria-label="Pause Recording" arrow><PauseIcon /></Tooltip></Button>
-          <Button id="endRecording" disabled={disabled.end} onClick={endRecording}><Tooltip title="End Recording" aria-label="End Recording" arrow><StopIcon /></Tooltip></Button>
-        </ButtonGroup>
-      </p>
+  render() {
 
-      <p>
-        <Link to={"/viewer/" + project}>{project}</Link>
-      </p>
+    let viewer = this.state.recorded ? <Viewer /> : null
+    let upload = this.state.edited ? <div>
+    <TextField label="Name your file" onChange={(evt) => { this.setState({ filename: evt.target.value }) }} />
+    <Button variant="contained" color="primary" id="download" onClick={() => this.download()}>Download</Button>
+  </div> : null
 
-      {/* <video controls muted id="video" autoPlay></video>
+    return (
+      <div className="App">
+        <br /><br />
+        <div>
+          <Button variant="contained" color="primary" id="enableRecording" disabled={this.state.disabled.enable} onClick={() => this.enableRecording()}>Enable Recording</Button>
+        </div>
+        <br />
+        <div>
+          <ButtonGroup variant="contained" color="primary">
+            <Button id="start" disabled={this.state.disabled.start} onClick={() => this.startRecording()}><Tooltip title="Start Recording" aria-label="Start Recording" arrow><FiberManualRecordIcon /></Tooltip></Button>
+            <Button id="pause" disabled={this.state.disabled.pause} onClick={() => this.pauseRecording()}><Tooltip title="Pause Recording" aria-label="Pause Recording" arrow><PauseIcon /></Tooltip></Button>
+            <Button id="endRecording" disabled={this.state.disabled.end} onClick={() => this.endRecording()}><Tooltip title="End Recording" aria-label="End Recording" arrow><StopIcon /></Tooltip></Button>
+          </ButtonGroup>
+        </div>
+
+
+        <Link to={"/viewer/" + this.state.project}>{this.state.project}</Link>
+
+
+        {/* <video controls muted id="video" autoPlay></video>
       <br></br> */}
 
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          Edit
-        </AccordionSummary>
-        <AccordionDetails>
-          <Viewer />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          Upload/Download
-        </AccordionSummary>
-        <AccordionDetails>
-          <p>
-            <TextField label="Name your file" onChange={(evt) => { setFilename(evt.target.value) }} />
-            <Button variant="contained" color="primary" id="download" onClick={download}>Download</Button>
-          </p>
-        </AccordionDetails>
-      </Accordion>
-    </div >
-  );
-}
+        {viewer}
 
-export default withRouter(Home);
+        {upload}
+
+      </div >
+    );
+  }
+}
