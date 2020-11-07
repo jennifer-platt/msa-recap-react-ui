bd08f7c6336fe71ebbdcfc847c7ff3139c87b2b5
jennifer-platt <jcp215@exeter.ac.uk>
2020-11-07 01:02:35 +0000

    Running test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,18 +4,6 @@ import App from './App';
 
 test('renders link to Home page', () => {
   const { getByText } = render(<App />);
-  const homeLink = getByText('Home');
+  const homeLink = getByText('Login');
   expect(homeLink).toBeInTheDocument();
-});
-
-test('renders link to About page', () => {
-  const { getByText } = render(<App />);
-  const aboutLink = getByText('About');
-  expect(aboutLink).toBeInTheDocument();
-});
-
-test('renders link to Viewer page', () => {
-  const { getByText } = render(<App />);
-  const viewerLink = getByText('Viewer');
-  expect(viewerLink).toBeInTheDocument();
 });
